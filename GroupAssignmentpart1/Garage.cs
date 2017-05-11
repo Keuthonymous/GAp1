ccc253cdd901f8d3ceddc267958b7e54514d325b
Wille Hansson <wille.multi@live.se>
2017-05-11 10:45:47 +0200

    fixed a thing

--- GroupAssignmentpart1/Garage.cs
+++ GroupAssignmentpart1/Garage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,6 +34,16 @@ namespace GroupAssignmentpart1
         internal IEnumerable<T> SearchByParkingDate(DateTime date, bool before)
         {
             throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator() //Did this VV
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException(); //To this ^^
         }
 
         public IEnumerable<T> Vehicles { get; private set; }

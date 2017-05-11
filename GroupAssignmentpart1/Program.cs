e1fc9b6dd95187970c8139643d1157c3d9f9382a
Wille Hansson <wille.multi@live.se>
2017-05-11 10:01:27 +0200

    menu, program.cs

--- GroupAssignmentpart1/Program.cs
+++ GroupAssignmentpart1/Program.cs
@@ -11,6 +11,31 @@ namespace GroupAssignmentpart1
         static void Main(string[] args)
         {
             //Initial commit
+            Menu();
+
         }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            switch (Console.ReadKey().KeyChar)
+            {
+                case '1': Console.Clear(); Console.WriteLine("Q) Exit"); switch (Console.ReadKey().KeyChar) { case 'q': ConfirmToMenu(); break; } break;
+                case 'q': ConfirmExit(); break;
+            }
+        }
+
+        static void ConfirmExit()
+        {
+            Console.WriteLine("Are you sure you want to return to menu?");
+            switch (Console.ReadKey().KeyChar)
+            {
+                case 'y': break;
+                case 'n': Menu(); break;
+            }
+        }
+            
+        
     }
 }

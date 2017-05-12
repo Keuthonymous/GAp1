53df64b53f2b2f5357de507fe4fb45896662776e
Keuthonymous <mikie.daughtrey@gmail.com>
2017-05-12 11:26:15 +0200

    Commit

--- GroupAssignmentpart1/Vehicle.cs
+++ GroupAssignmentpart1/Vehicle.cs
@@ -125,10 +125,10 @@ namespace GroupAssignmentpart1
         {
             return string.Join(Constants.MENU_ITEMS_SEPARATOR.ToString(),
                                 new string[]{ this.GetType().Name,
-                                              thisTI.ToTitleCase(color),
+                                              registrationPlate.ToUpper(),
                                               thisTI.ToTitleCase(brand),
                                               thisTI.ToTitleCase(model),
-                                              registrationPlate.ToUpper(),
+                                              thisTI.ToTitleCase(color),
                                               numOfDoors.ToString(),
                                               numberOfWheels.ToString(),
                                               parkingTime.ToString() });
